4fce3a6d71977b2f0b398847053a17a99df52730
Germán Lugo <g.lugo.dev@gmail.com>
2022-04-20 14:56:00 +0200

    Update to import openzeppelin.

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,13 @@
  *
  */
 
+require("dotenv").config()
+const fs = require('fs');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
 const infuraApiKey = fs.readFileSync(".infuraApiKey").toString().trim();
+const moralisApiKey = fs.readFileSync(".infuraApiKey").toString().trim();
 
 module.exports = {
   /**
@@ -34,32 +36,19 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
+  plugins: ["truffle-plugin-verify"],
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "5777",       // Any network (default: none)
+      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      // from: <address>,        // Account to send txs from (default: accounts[0])
+      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
     ropsten: {
-    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApiKey}`),
+      provider: () => new HDWalletProvider( mnemonic, `https://ropsten.infura.io/v3/${infuraApiKey}` ),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
@@ -67,13 +56,31 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     goerli: {
-      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraApiKey}`),
-        network_id: 5,       // Ropsten's id
-        gas: 4465030,        // Ropsten has a lower block limit than mainnet
-        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-      },
+      provider: () => new HDWalletProvider( mnemonic, `https://goerli.infura.io/v3/${infuraApiKey}` ),
+      network_id: 5,       // Goerli's id
+      gas: 4465030,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider( mnemonic, `https://polygon-mumbai.infura.io/v3/${infuraApiKey}`),
+      network_id: 80001,
+      confirmations: 2,
+      skipDryRun: true,
+    },
+    polygon: {
+      provider: () => new HDWalletProvider( mnemonic, `https://polygon-mainnet.infura.io/v3/${infuraApiKey}`),
+      network_id: 137,
+      confirmations: 2,
+      skipDryRun: true,
+    },
+    avalanche_fuji: {
+      provider: () => new HDWalletProvider( mnemonic, `https://speedy-nodes-nyc.moralis.io/${moralisApiKey}/avalanche/testnet`),
+      network_id: 43113,
+      confirmations: 2,
+      skipDryRun: true,
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -90,7 +97,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
