abc5c4e0df6498529a144e19ed0894b5398d4957
Germán Lugo <g.lugo.dev@gmail.com>
2022-05-09 16:04:24 +0200

    Improve Schema.

--- Schemas/User.js
+++ Schemas/User.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
-  username : { type: String, required: true, unique: true },
-  sinceMessageID : { type: Number, required: true },
+  username : { type: String, required: false, unique: true },
   telegramID: { type: Number, required: true },
   name: { type: String, required: false },
   language: { type: String, required: false },
   referer: { type: Number, required: false },
-  session: { type: Number, default: 1 }, 
+  email: { type: String, required: false },
+  phone: { type: Number, required: false },
+  VCS: { type: String, required: false },
+  phoneCode: { type: Number },
+  emailCode: { type: Number },
+  verifiedPhone: { type: Boolean, default: false },
+  verifiedEmail: { type: Boolean, default: false },
 });
 
 module.exports = model('User', UserSchema)
+
