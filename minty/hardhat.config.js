f05506ffd65eb9aa8a168732f6c06e378029857f
Germán Lugo <g.lugo.dev@gmail.com>
2022-06-02 19:38:29 +0200

    Deploy to mumbai.

--- minty/hardhat.config.js
+++ minty/hardhat.config.js
@@ -1,4 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-waffle")
+// require("@nomiclabs/hardhat-ganache")
+require('@nomiclabs/hardhat-ethers')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -10,6 +12,9 @@ module.exports = {
     networks: {
         hardhat: {},
         localhost: {},
+        matic: {
+            url: "https://polygon-mumbai.infura.io/v3/e9b69e7a1e73427396eb9b8204dbea02",
+            accounts: ["93d999433c32df2c40cd444f03b2972d7b134dd58bfce1e08b470eae6cbcc198"]
+        },
     }
-};
-
+}
--- minty/migrations/2_deployment.js
+++
@@ -1,5 +0,0 @@
-const Minty = artifacts.require("Minty")
-
-module.exports = (deployer) => {
-  deployer.deploy(Minty)
-}
