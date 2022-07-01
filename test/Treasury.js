c09dc28022e99227bd79389b84e6850f9d6a4395
Germán Lugo <g.lugo.dev@gmail.com>
2022-07-01 09:42:36 +0200

    Fix tests.

--- test/Treasury.js
+++ test/Treasury.js
@@ -1,3 +1,5 @@
+const { web3 } = require('../utils/web4u')
+
 const Treasury = artifacts.require("Treasury")
 
 require('chai')
@@ -12,7 +14,6 @@ contract("Treasury", (accounts) => {
 
   describe('First round of test', async () => {
     it("Should assert msg.sender is owner and treasuryBox", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const owner = await TreasuryInstance.owner()
       const TreasuryBox = await TreasuryInstance.treasuryBox()
 
@@ -20,14 +21,12 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if smart contract is not paused", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const isPaused = await TreasuryInstance.paused()
 
       assert.equal( isPaused, false, "Smart contract isn't paused." )
     })
 
     it("Should assert referer of owner is smart contract self", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const owner = await TreasuryInstance.owner()
       const address = TreasuryInstance.address
       const referer = await TreasuryInstance.referer(owner)
@@ -36,7 +35,6 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if referer changes correctly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.setReferer(accounts[0], accounts[1])
       await TreasuryInstance.setReferer(accounts[1], accounts[2])
       await TreasuryInstance.setReferer(accounts[2], accounts[3])
@@ -57,7 +55,6 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if referer changes correctly 2", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       let err
       try {
         await TreasuryInstance.setReferer(accounts[9], accounts[1])
@@ -70,7 +67,6 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if TreasuryBox changes correctly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.setTreasuryBox(accounts[2])
       const treasuryBox = await TreasuryInstance.treasuryBox()
 
@@ -80,7 +76,6 @@ contract("Treasury", (accounts) => {
     //TODO: Assert if that new account is payable as well. 
 
     it("Should assert if TreasuryBox changes correctly 2", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.setTreasuryBox(accounts[0])
       const treasuryBox = await TreasuryInstance.treasuryBox()
 
@@ -88,20 +83,18 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if Outsourcing pay TreasuryBox", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const treasuryBox = await TreasuryInstance.treasuryBox()
       const balance_1 = await web3.eth.getBalance(treasuryBox)
       const amount = '5000000000000000000'
       await TreasuryInstance.outSourcing({ from: accounts[9], value: amount })
       const balance_2 = await web3.eth.getBalance(treasuryBox)
 
-      assert.equal(amount,  web3.utils.toBN(balance_2) - web3.utils.toBN(balance_1), "Outsourcing pay TreasuryBox properly.")
+      assert.equal(amount,  balance_2 - balance_1, "Outsourcing pay TreasuryBox properly.")
     })
 
     it("Should assert if TreasuryBalance rises in outsourcing account", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const balance_1 = await TreasuryInstance.treasuryBalanceOf(accounts[8])
-      const amount = '1000000000000' 
+      const amount = '1000000000000000000' 
       await TreasuryInstance.outSourcing({ from: accounts[8], value: amount })
       const balance_2 = await TreasuryInstance.treasuryBalanceOf(accounts[8])
 
@@ -109,10 +102,9 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if assignment works properly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const treasuryBox = await TreasuryInstance.treasuryBox()
       const balance_1 = await web3.eth.getBalance(treasuryBox)
-      const amount = 1000000000000000000
+      const amount = '1000000000000000000'
       await TreasuryInstance.assignment({ from: accounts[14], value: amount })
       const balance_2 = await web3.eth.getBalance(treasuryBox)
 
@@ -120,10 +112,9 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if assignment pays upstream in properly proportion", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const balance_1 = await TreasuryInstance.treasuryBalanceOf(accounts[0])
       const balance_2 = await TreasuryInstance.treasuryBalanceOf(accounts[1])
-      await TreasuryInstance.assignment({ from: accounts[9], value: '1000000000000' })
+      await TreasuryInstance.assignment({ from: accounts[9], value: '1000000000000000000' })
       const balance_3 = await TreasuryInstance.treasuryBalanceOf(accounts[0])
       const balance_4 = await TreasuryInstance.treasuryBalanceOf(accounts[1])
 
@@ -131,17 +122,16 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if assignment pays upstream properly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       const balance_1 = await TreasuryInstance.treasuryBalanceOf(accounts[2])
-      const amount = 1000000000000 / 100
-      await TreasuryInstance.assignment({ from: accounts[9], value: '1000000000000' })
+      const amount = '5550000000000000000'
+      await TreasuryInstance.assignment({ from: accounts[9], value: amount })
       const balance_2 = await TreasuryInstance.treasuryBalanceOf(accounts[2])
+      const fee = web3.utils.toBN(amount / 100)
 
-      assert.equal(balance_1, balance_2 - amount , "Assignment pays upstream properly.")
+      assert.equal(balance_1, balance_2 - fee, "Assignment pays upstream properly.")
     })
 
     it("Should assert if pauses changes correctly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.pause({ from: accounts[0] })
       const isPaused = await TreasuryInstance.paused()
       
@@ -149,7 +139,6 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if pauses revert correctly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.unpause({ from: accounts[0] })
       const isNotPaused = await TreasuryInstance.paused()
       
@@ -157,27 +146,23 @@ contract("Treasury", (accounts) => {
     })
 
     it("Should assert if withdraw works properly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
-      const treasuryBalance_1 = await TreasuryInstance.treasuryBalance()
-      const amount = 5000000000
-      await TreasuryInstance.withdrawFrom(amount, accounts[0], { from: accounts[0] })
-      const treasuryBalance_2 = await TreasuryInstance.treasuryBalance()
+      const amount = await TreasuryInstance.treasuryBalanceOf(accounts[9], { from: accounts[0] })
+      await TreasuryInstance.withdrawFrom(amount, accounts[9], { from: accounts[0] })
+      const treasuryBalance = await TreasuryInstance.treasuryBalanceOf(accounts[9], { from: accounts[0] })
 
-      assert.equal(parseInt(treasuryBalance_1), parseInt(treasuryBalance_2) + amount, "with pays treasury properly.")
+      assert.equal(treasuryBalance, 0, "withdraw works properly.")
     })
 
     it("Should assert if withdraw works properly 2", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
-      const userBalance_1 = await TreasuryInstance.treasuryBalanceOf(accounts[5])
-      const amount = 5000000000
-      await TreasuryInstance.withdrawFrom(amount, accounts[5], { from: accounts[0] })
-      const userBalance_2 = await TreasuryInstance.treasuryBalanceOf(accounts[5])
+      const balance_1 = await TreasuryInstance.treasuryBalance()
+      const amount = await TreasuryInstance.treasuryBalanceOf(accounts[1], { from: accounts[0] })
+      await TreasuryInstance.withdrawFrom(amount, accounts[2], { from: accounts[0] })
+      const balance_2 = await TreasuryInstance.treasuryBalance()
 
-      assert.equal(parseInt(userBalance_1), parseInt(userBalance_2) + amount, "withdraw works properly.")
+      assert.equal(balance_1 - amount, balance_2, "withdraw works properly 2.")
     })
 
     it("Should assert if owner changes correctly", async () => {
-      // const TreasuryInstance = await Treasury.deployed()
       await TreasuryInstance.transferOwnership(accounts[1])
       const owner = await TreasuryInstance.owner()
 
