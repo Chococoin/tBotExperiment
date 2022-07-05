6ddebc797ef9d068e9ccc3cc2e594866c5130a74
Germán Lugo <g.lugo.dev@gmail.com>
2022-07-06 00:21:01 +0200

    Clean syntax.

--- utils/generateAddresses.js
+++ utils/generateAddresses.js
@@ -3,5 +3,5 @@ const ethers = require('ethers')
 module.exports  = function generateAddresses(mnemonic) {
 
   const wallet = ethers.Wallet.fromMnemonic(mnemonic)
-  return wallet.address;
+  return wallet.address
 }
