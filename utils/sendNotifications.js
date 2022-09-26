21e553ceb2bbd3214d3b03cac8bfde49ea375455
Germán Lugo <g.lugo.dev@gmail.com>
2022-09-26 08:11:16 +0200

    Send NFT notification.

--- utils/sendNotifications.js
+++ utils/sendNotifications.js
@@ -9,6 +9,8 @@ const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 const sms = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 const TWILIO_PHONE = process.env.TWILIO_PHONE
 
+const User = require('../Schemas/User.js')
+
 async function sendNotifications (mail, phone) {
 
   let email = ( mail ) => {
@@ -77,4 +79,51 @@ async function sendNTFNotifications (mail, phone, NFTdata) {
     .catch((error) => {console.error(error)})
 }
 
-module.exports = { sendNotifications , sendNTFNotifications }
+async function sendPollNotifications ( userCreator, phone, NFTdata ) {
+  
+  const users = await User.find({})
+  let email = ( mail ) => {
+    let msg = {
+      to: mail,
+      from: SENDGRID_VALID_EMAIL,
+      subject: `ChocoCryptoBot: ${userCreator.username} have created a new poll.`,
+      html: '<h1>A new poll was created for your DAO</h1>' 
+      // '<h3>TOKEN ID:</h3>' +
+      // `<p>${NFTdata.tokenId}</p>` +
+      // '<h3>OWNER ADDRESS</h3>' +
+      // `<p>${NFTdata.ownerAddress}</p>` +
+      // '<h3>NFT TITLE</h3>' +
+      // `<p>${NFTdata.metadata.name}</p>` +
+      // '<h3>NFT DESCRIPTION</h3>' +
+      // `<p>${NFTdata.metadata.description}</p>` +
+      // '<h3>NFT IMAGE</h3>' +
+      // `<p>${NFTdata.metadata.image}</p>` +
+      // '<h3>NFT METADATA</h3>' +
+      // `<p>${NFTdata.metadataGatewayURL}</p>`
+    }
+    return msg
+  }
+  users.forEach((user) => {
+    console.log("Sending poll notification to:", user.email)
+    const msg = email( user.email )
+    sgMail.send(msg)
+    .then(console.log( "Email sent"))
+    .catch((error) => {console.error(error)})
+  })
+
+
+  // sms.messages
+  //   .create({
+  //     body: `Your have mint an NFT title ${ NFTdata.metadata.name }. For more info check your email.`,
+  //     from: `${ TWILIO_PHONE }`,
+  //     to: '+39' + phone
+  //   })
+  //   .then(message => console.log(message.sid))
+  //   .catch((error) => {console.error(error)})
+
+  // sgMail.send(msg)
+  //   .then(console.log( "Email sent"))
+  //   .catch((error) => {console.error(error)})
+}
+
+module.exports = { sendNotifications , sendNFTNotifications, sendPollNotifications }
